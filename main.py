bb65323db13b2864be3831d861497f8db2296555
Sam May <samrmay@sas.upenn.edu>
2021-02-21 19:29:35 -0500

    add generate plot function

--- main.py
+++ main.py
@@ -17,10 +17,20 @@ def get_mo_range(out):
     return int(orb_min), int(orb_max)
 
 
+def generate_plot(p, mo, op, f, pt=1, ngrid=40):
+    plot_type = '\n'.join(['1', str(pt)]) + '\n'
+    mo_op = '\n'.join(['2', str(mo), '3', str(op)]) + '\n'
+    format_ = '\n'.join(['5', str(f)]) + '\n'
+    resolution = '\n'.join(['4', str(ngrid)]) + '\n'
+    in_dat = plot_type + mo_op + format_ + resolution + '10\n'
+    p.stdin.write(in_dat.encode())
+
+
 def main():
     cmd = ['orca_plot', 'FeFeTHF+1_9_casscf_022121_locAS.gbw', '-i']
-    p = Popen(cmd, stdout=PIPE, stdin=PIPE)
-    p.communicate(input='5\n7\n10'.encode())
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE)
+    generate_plot(p, 139, 0, 7)
+    p.stdin.write('11\n'.encode())
 
 
 main()
