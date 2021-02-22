a66ce3c5c708b6bf73b1f45be9877a35afcea0b0
Sam May <samrmay@sas.upenn.edu>
2021-02-21 21:43:13 -0500

    add dicts and use arguments

--- main.py
+++ main.py
@@ -40,7 +40,14 @@ def get_mo_range(out):
     return int(orb_min), int(orb_max)
 
 
-def generate_plot(p, mo, op, f, pt=1, ngrid=40):
+PLOT_TYPE_DICT = {'mo': 1, 'electron_density': 2, 'spin_density': 3,
+                  'natural_orbitals': 4, 'corrosponding_orbitals': 5,
+                  'atomic_orbitals': 6}
+
+FORMAT_DICT = {'.cube': 7}
+
+
+def generate_plot(p, mo, op, f=7, pt=1, ngrid=40):
     plot_type = '\n'.join(['1', str(pt)]) + '\n'
     mo_op = '\n'.join(['2', str(mo), '3', str(op)]) + '\n'
     format_ = '\n'.join(['5', str(f)]) + '\n'
@@ -49,17 +56,19 @@ def generate_plot(p, mo, op, f, pt=1, ngrid=40):
     p.stdin.write(in_dat.encode())
 
 
-def main(basename):
+def main():
+    basename, out_f, mo_in_ext, ngrid, pt = parse_args()
+
     out_file = basename + '.out'
-    mo_file = basename + '.gbw'
+    mo_file = basename + mo_in_ext
     cmd = ['orca_plot', mo_file, '-i']
 
     orb_min, orb_max = get_mo_range(out_file)
 
     p = Popen(cmd, stdin=PIPE, stdout=PIPE)
     for mo in range(orb_min, orb_max + 1):
-        generate_plot(p, mo, 0, 7)
+        generate_plot(p, mo, 0, FORMAT_DICT[out_f], PLOT_TYPE_DICT[pt], ngrid)
     p.stdin.write('11\n'.encode())
 
 
-main('FeFeTHF+1_9_casscf_022121_locAS')
+main()
